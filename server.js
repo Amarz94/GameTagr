93b990df26c4566956c4466291cce4a2fe1242ad
rpc08002 <rcurtin0491@gmail.com>
2021-02-13 13:28:50 -0500

    created route folders, created model.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express');
 
 const PORT = process.env.PORT || 3000;
@@ -18,8 +20,9 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them.
-const routes = require('./controllers/burgers_controller.js');
+const routes = require('./controllers/game_controller.js');
 
 app.use(routes);
 
 app.listen(PORT, () => console.log(`App now listening at localhost:${PORT}`));
+
