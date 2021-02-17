ed19e56066d0630b2c2e6e187c464dc6db3c88f5
Cris Franco <cris.franco219@gmail.com>
2021-02-16 20:03:43 -0500

    fixed templating for example data in games handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 // Sequelize instance to connect to the database
 const Sequelize = require("sequelize");
-module.exports = new Sequelize("game_db", "root", "15Feb19!123", {
+module.exports = new Sequelize("game_db", "root", process.env.MY_PASSWORD, {
     host: "localhost",
     dialect: "mysql"
 });
--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.set("view engine", "handlebars");
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => res.send("INDEX"));
+//app.get("/", (req, res) => res.send("INDEX"));
 
 // Game routes
 app.use("/games", require("./routes/games"));
