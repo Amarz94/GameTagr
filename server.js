a6621efac28518729eddade2309317ea2b44d294
rpc08002 <rcurtin0491@gmail.com>
2021-02-18 19:47:40 -0500

    changed layout on homepage, removed nav bar from main template, added search bar

--- server.js
+++ server.js
@@ -35,32 +35,3 @@ app.use("/games", require("./routes/games"));
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
-
-// require('dotenv').config()
-
-// import express, { static, urlencoded, json } from 'express';
-
-// const PORT = process.env.PORT || 3000;
-
-// const app = express();
-
-// Serve static content for the app from the "public" directory in the application directory.
-// app.use(static('public'));
-
-// Parse request body as JSON
-// app.use(urlencoded({ extended: true }));
-// app.use(json());
-
-// Set Handlebars.
-// import exphbs from 'express-handlebars';
-
-// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// app.set('view engine', 'handlebars');
-
-// Import routes and give the server access to them.
-// import routes from './controllers/game_controller.js';
-
-// app.use(routes);
-
-// app.listen(PORT, () => console.log(`App now listening at localhost:${PORT}`));
-//test
